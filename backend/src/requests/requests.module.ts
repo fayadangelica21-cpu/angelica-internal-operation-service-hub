6b2269d0eb58de53d71f8cbcf8f495f5263caeb8
fayadangelica21-cpu <fayad.angelica21@gmail.com>
2026-09-16 20:19:03 +0300

    Add TypeOrmModule import and configure RequestEntity in RequestsModule

--- backend/src/requests/requests.module.ts
+++ backend/src/requests/requests.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RequestEntity } from './entities/request.entity';
+import { RequestStateMachineService } from './request-state-machine.service';
 import { RequestsController } from './requests.controller';
 import { RequestsService } from './requests.service';
-import { RequestStateMachineService } from './request-state-machine.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RequestEntity])],
   controllers: [RequestsController],
   providers: [RequestsService, RequestStateMachineService],
   exports: [RequestsService, RequestStateMachineService],
