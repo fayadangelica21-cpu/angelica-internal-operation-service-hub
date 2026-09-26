7e048a5603954c50d172f68fc9eb54f4311f5aca
fayadangelica21-cpu <fayad.angelica21@gmail.com>
2026-09-26 17:06:45 +0300

    feat: update requests module

--- backend/src/requests/requests.module.ts
+++ backend/src/requests/requests.module.ts
@@ -7,9 +7,10 @@ import { RequestsService } from './requests.service';
 import { TriageController } from '../triage/triage.controller';
 import { TriageProviderService } from '../triage/triage-provider.service';
 import { TriageService } from '../triage/triage.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RequestEntity])],
+  imports: [TypeOrmModule.forFeature([RequestEntity]), AuthModule],
   controllers: [RequestsController, TriageController],
   providers: [RequestsService, RequestStateMachineService, TriageService, TriageProviderService],
   exports: [RequestsService, RequestStateMachineService, TriageService, TriageProviderService],
